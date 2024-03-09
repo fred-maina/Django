c74b5dccd79cd61ec32703218e1ece03d3bc5c5a
fred-maina <fchege04@gmail.com>
2024-03-09 13:35:59 +0300

    initital commit

---
+++ hello/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse #import the Http response class
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    return render(request,"hello/index.html")
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+def greet(request,name):
+    return render(request,"hello/greet.html",{"name":name.capitalize()})
