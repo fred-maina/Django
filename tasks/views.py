28921f9316e1b98167d899eff6a6be2ebb0ee5e5
fred-maina <fchege04@gmail.com>
2024-03-09 17:57:01 +0300

    Added cookies

--- tasks/views.py
+++ tasks/views.py
@@ -3,25 +3,27 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
-tasks=[]
+
 class NewTaskForm(forms.Form):
      task= forms.CharField(label="New Task")
 
 
 def index(request):
-     return render(request, "tasks/index.html",{
-        "tasks":tasks
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request, "tasks/index.html",{
+        "tasks":request.session["tasks"]
     })
 def add(request):
-     if request.method == "POST":
+    if request.method == "POST":
         form=NewTaskForm(request.POST)
         if form.is_valid():
              task=form.cleaned_data["task"]
-             tasks.append(task)
+             request.session["tasks"] += [task]
              return HttpResponseRedirect(reverse("tasks:index"))
         else:
              return render (request,"tasks/add.html",{
                   "form":form
              })
 
-     return render(request,"tasks/add.html",{"form":NewTaskForm})
+    return render(request,"tasks/add.html",{"form":NewTaskForm})
